f72e5a98a856b652ed98b3a2425b2e19395f652c
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-31 14:30:15 -0400

    Fix linter errors

--- app/scripts/pets.js
+++ app/scripts/pets.js
@@ -1,8 +1,8 @@
-'use strict';
+import Ember from 'ember';
 
 const Pet = Ember.Object.extend({
-  eat: () => { console.log('nom nom nom') },
-  sleep: () => { console.log('zzz') },
+  eat: () => { console.log('nom nom nom'); },
+  sleep: () => { console.log('zzz'); },
 });
 
 let bruce = Pet.create({
@@ -10,13 +10,17 @@ let bruce = Pet.create({
   age: 9,
 });
 
+bruce.get('age');
 bruce.set('age', 10);
+bruce.get('age');
 
 const Dog = Pet.extend({
-  bark: () => { console.log('arf') },
+  bark: () => { console.log('arf'); },
 });
 
 let jellybeans = Dog.create({
   name: 'Jellybeans',
   age: 7,
 });
+
+jellybeans.bark();
